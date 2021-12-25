1718e75a98638a4c012b5dac1582fa823ea2f5d8
Dmitriy Melnik <ml.dmitriymelnik@yandex.ru>
2021-12-25 23:00:15 +0300

    add log http handler method

--- api/internal/apiserver/server.go
+++ api/internal/apiserver/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"net/http"
+	"time"
 )
 
 type server struct {
@@ -25,10 +26,46 @@ func newServer(store store.Store) *server {
 }
 
 func (s *server) configureRouter() {
+	s.router.Use(s.logRequest)
 	s.router.HandleFunc("/test", s.handleUsersCreate()).Methods("GET")
 }
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.router.ServeHTTP(w, r)
+}
+
+const (
+	ctxKeyRequestID = iota
+)
+
+func (s *server) logRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := s.logger.WithFields(logrus.Fields{
+			"remote_addr": r.RemoteAddr,
+			"request_id":  r.Context().Value(ctxKeyRequestID),
+		})
+		logger.Infof("started %s %s", r.Method, r.RequestURI)
+
+		start := time.Now()
+		rw := &responseWriter{w, http.StatusOK}
+		next.ServeHTTP(rw, r)
+
+		var level logrus.Level
+		switch {
+		case rw.code >= 500:
+			level = logrus.ErrorLevel
+		case rw.code >= 400:
+			level = logrus.WarnLevel
+		default:
+			level = logrus.InfoLevel
+		}
+		logger.Logf(
+			level,
+			"completed with %d %s in %v",
+			rw.code,
+			http.StatusText(rw.code),
+			time.Now().Sub(start),
+		)
+	})
 }
 
 type Dog struct {
