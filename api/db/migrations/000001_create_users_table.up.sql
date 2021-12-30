42ce3257e82afc7024be2c425543aa2ed1c5608d
Dmitriy Melnik <ml.dmitriymelnik@yandex.ru>
2021-12-30 17:41:30 +0300

    Fix users_token and users migrations

--- api/db/migrations/000001_create_users_table.up.sql
+++ api/db/migrations/000001_create_users_table.up.sql
@@ -2,5 +2,7 @@ CREATE TABLE IF NOT EXISTS users(
     id bigserial primary key unique,
     username VARCHAR (50) unique,
     password VARCHAR (50) not null,
-    email VARCHAR (300) unique not null
+    email VARCHAR (300) unique not null,
+    created_at      timestamp not null,
+    updated_at      timestamp not null
 );
